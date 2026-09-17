cb12739de8465c0a1209d0bf4b251deacd916a1e
EQWS-creator <sbinance508@gmail.com>
2026-09-17 14:56:24 +0100

    Modify tracking data for EWS100001 and EWS100002
    Updated the destination and current location for EWS100001, and corrected the formatting for EWS100002.

--- tracking-data.js
+++ tracking-data.js
@@ -1,11 +1,11 @@
 EWS100001: {
   status: "In Transit",
   origin: "New York City, USA",
-  destination: "Holy Karbala, Iraq",
+  currentLocation: "Baghdad, Iraq",
+  destination: "Karbala, Iraq",
   service: "International Express"
 }
-
-  EWS100002: {
+ EWS100002: {
   status: "Deliver",
   origin: "Baghdad international airport, IRAQ",
   destination: "Holy Karbala, Iraq",

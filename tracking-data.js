6571855344cd7e5cc4811740bc856f757fb69d51
EQWS-creator <sbinance508@gmail.com>
2026-09-17 13:36:07 +0100

    Create tracking-data.js
    Equal-World-Shipping/
    ├── index.html
    ├── tracking.html
    ├── style.css
    ├── script.js
    └── tracking-data.js

---
+++ tracking-data.js
@@ -0,0 +1,17 @@
+const shipments = {
+  "EQWS123456": {
+    status: "In Transit",
+    origin: "New York, USA",
+    destination:holy karbala ", Iraq",
+    location: "Iraq international airport, Iraq",
+    estimatedDelivery: "September 17, 2026"
+  },
+
+  "EQWS789012": {
+    status: "Delivered",
+    origin: "New York, USA",
+    destination: "iraq, iraq",
+    location: iraq", Iraq",
+    estimatedDelivery: "September 18, 2026"
+  }
+};
